fcb2f0237d373126349409a413f36cfc12ee3aa9
zakariaLj <zakariaoulji@hotmail.fr>
2020-02-02 14:03:08 +0100

    commit exo7

---
+++ exo1.js
@@ -0,0 +1,6 @@
+let Exo1 = document.getElementById("exo1");
+let Section2 = document.getElementById("section2");
+let buttonH2 = document.getElementById("tryIt");
+
+
+buttonH2.addEventListener('click',()=>{document.querySelector('h2').style.backgroundColor = 'red';})
---
+++ main.js
@@ -0,0 +1,7 @@
+import "./exo1.js";
+import "./exo2.js";
+import "./exo3.js";
+import "./exo4.js";
+import "./exo5.js";
+import "./exo6.js";
+import "./exo7.js";
